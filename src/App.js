1c6f43c4ab49b7fb9e0e245f7a5a2217bf3898e6
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-05-24 18:05:31 -0400

    Setting up our React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import './App.css';
+import Routes from "./Routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Container>
+            <Navbar fluid collapseOnSelect expand="lg" bg="light" variant="light">
+              <Navbar.Brand>
+                <Link to="/">Scratch</Link>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Item>
+
+                            <Nav.Link tag={Link} to="/signup">Signup</Nav.Link>
+
+                    </Nav.Item>
+                    <Nav.Item>
+
+                            <Nav.Link tag={Link} to="/login">Login</Nav.Link>
+
+                    </Nav.Item>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+        </Container>
+        <Routes />
+    </Router>
   );
 }
 
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "thor-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://2sdl1m0nj9.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_yTeqUShDJ",
+    APP_CLIENT_ID: "6tdug74qn2uhf84kgac05hfnqk",
+    IDENTITY_POOL_ID: "us-east-1:ae4ff3b5-3b8d-42da-a12d-3e6a8378ae37"
+  }
+};
